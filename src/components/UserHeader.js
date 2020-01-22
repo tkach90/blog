c8fdcc6aecb9e060800b51ae299b7097cbb3f66c
thach90 <tkach90@yahoo.com.ua>
2020-01-23 01:20:41 +0200

    delete fetch duplications using lodash uniq function

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
-    componentDidMount() {
-        this.props.fetchUser(this.props.userId);
-    }
-
     render() {
         const { user } = this.props;
 
@@ -23,6 +18,5 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 export default connect(
-    mapStateToProps,
-    { fetchUser }
+    mapStateToProps
 )(UserHeader);
